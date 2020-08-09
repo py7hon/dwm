deeb4bd44d95141ce7ae47e141cc99e9ac2f147d
Iqbal Rifai <yukifag@pm.me>
2020-08-09 22:45:04 +0700

    set gappx to 7

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 /* #262626 */
 /* appearance */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
-static const unsigned int gappx     = 6;        /* gaps between windows */
+static const unsigned int gappx     = 7;        /* gaps between windows */
 static const unsigned int snap      = 0;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
